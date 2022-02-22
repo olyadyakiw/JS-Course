682b1fb0174c223c4227e28b4d7fcc657021c17d
olya <olya.dyakiv26@gmail.com>
2022-02-22 15:17:16 +0200

    start course

---
+++ 12/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+
+const user = "Olya";
+
+alert(`Привет, ${user}`);
---
+++ 5/js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+
+var nam = 'Olya';
+
+{
+    var result = 50;
+}
+
+console.log(result);
+
+alert(5);
+[].push('a');
